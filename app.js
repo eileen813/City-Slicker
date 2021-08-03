0660857ad0ee5916d12808a563393513052d039e
eileen813 <eileenolivera@hotmail.com>
2021-08-03 11:52:23 -0400

    updated with commented outs

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const DOMAIN = `https://api.teleport.org/api/cities/`;
-const submitButton = document.querySelector("#get-city")
+const DOMAIN = `https://api.teleport.org/api/cities/`
+// const DATA_URL = `https://api.teleport.org/api/cities/?search=&embed=city:search-results/city:item/city:country&embed=city:search-results/city:item/city:admin1_division&embed=city:search-results/city:item/city:urban_area&embed=city:search-results/city:item/city:timezone/tz:offsets-now`
+// const DETAILS_URL = `https://api.teleport.org/api/urban_areas/slug:{CITY HERE}/details/`
 
+const submitButton = document.querySelector("#get-city")
 
 async function getData() {
   try {
     const findCity = document.querySelector("#city-search").value
-      // console.log(findCity)
+    // console.log(findCity)
     const response = await axios.get(`${DOMAIN}?search=${findCity}`)
-      console.log(response.data)
+    // console.log(response.data)
+    const cityData = response.data
+    showCityData(cityData)
+    
+    return cityData
   } catch (error) {
-      console.error(error)
+    console.error(error)
   }
-}
+  // document.getElementById(`#city-search`) = findCity.data
+  }
+
+  // function showCityData(data) {
+  //     // console.log(data)
+  //   const cityDiv = document.createElement("div")
+  //   findCity.append(cityDiv)
+  // }
 
 submitButton.addEventListener("click", (e) => {
   e.preventDefault()
