893b434d49cbf5084bcacb00bdc6750d8b156ae0
eileen813 <eileenolivera@hotmail.com>
2021-08-03 16:42:33 -0400

    updated elements & some styling

--- app.js
+++ app.js
@@ -1,32 +1,41 @@
-const DOMAIN = `https://api.teleport.org/api/cities/`
+// const DOMAIN = `https://api.teleport.org/api/cities/`
 // const DATA_URL = `https://api.teleport.org/api/cities/?search=&embed=city:search-results/city:item/city:country&embed=city:search-results/city:item/city:admin1_division&embed=city:search-results/city:item/city:urban_area&embed=city:search-results/city:item/city:timezone/tz:offsets-now`
-// const DETAILS_URL = `https://api.teleport.org/api/urban_areas/slug:{CITY HERE}/details/`
-
-const submitButton = document.querySelector("#get-city")
-
-async function getData() {
+// const DETAILS_URL = `https://api.teleport.org/api/urban_areas/slug:${cityname}/details/`
+const form = document.querySelector('#city-form')
+const cityList = document.querySelector(`#city-data`)
+// 
+async function getData(cityname) {
   try {
-    const findCity = document.querySelector("#city-search").value
-    // console.log(findCity)
-    const response = await axios.get(`${DOMAIN}?search=${findCity}`)
-    // console.log(response.data)
-    const cityData = response.data
-    showCityData(cityData)
-    
-    return cityData
+    const cityDetails = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${cityname}/details/`)
+    buildElements(cityDetails.data)
+    return cityDetails
   } catch (error) {
     console.error(error)
   }
-  // document.getElementById(`#city-search`) = findCity.data
-  }
-
-  // function showCityData(data) {
-  //     // console.log(data)
-  //   const cityDiv = document.createElement("div")
-  //   findCity.append(cityDiv)
-  // }
-
-submitButton.addEventListener("click", (e) => {
+}
+// getData('seattle')
+form.addEventListener('submit', (e) => {
   e.preventDefault()
-  getData()
+  const inputValue = document.querySelector('#city-search').value
+  // console.log(inputValue)
+  let sanitizeData = inputValue.trim().replaceAll(" ", '-').toLowerCase()
+  console.log(sanitizeData)
+  getData(sanitizeData)
 })
+function buildElements(data) {
+  let cityElements = `
+  <p>${data}(categories[1].data[0].float_value.Population)population</p>
+  `
+  document.querySelector('#city-data').insertAdjacentHTML('beforeend', cityElements)
+
+  // data.forEach((city) => {
+  // console.log("Build Elements", data)
+  // const cityDiv = document.createElement("div")
+  //   cityList.append(cityDiv)
+    
+  // const cityPop = document.createElement("p")
+  //   cityPop.textContent = categories[1].data[0].float_value.Population
+  //   cityList.append(cityPop)
+  // })
+  // return data
+}
