1bb9b3698a4a19ef63ca0fb740409c6bcc5f3004
eileen813 <eileenolivera@hotmail.com>
2021-08-03 21:37:14 -0400

    updated styling & added elements

--- app.js
+++ app.js
@@ -4,10 +4,11 @@
 const form = document.querySelector('#city-form')
 const cityList = document.querySelector(`#city-data`)
 // 
-async function getData(cityname) {
+async function getData(cityName) {
+  removeElement(cityList)
   try {
-    const cityDetails = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${cityname}/details/`)
-    buildElements(cityDetails.data)
+    const cityDetails = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${cityName}/details/`)
+    buildElements(cityDetails.data.categories)
     return cityDetails
   } catch (error) {
     console.error(error)
@@ -22,20 +23,53 @@ form.addEventListener('submit', (e) => {
   console.log(sanitizeData)
   getData(sanitizeData)
 })
-function buildElements(data) {
-  let cityElements = `
-  <p>${data}(categories[1].data[0].float_value.Population)population</p>
-  `
-  document.querySelector('#city-data').insertAdjacentHTML('beforeend', cityElements)
+
+function buildElements(cityData) {
+console.log("Build Elements", cityData)
+const cityDiv = document.createElement("div")
+  cityList.append(cityDiv)
+
+  const cityPop = document.createElement("p")
+  cityPop.textContent = `Urban area population in millions is ${cityData[1].data[0].float_value}`
+  cityDiv.append(cityPop)
+
+  const cityHigh = document.createElement("p")
+  cityHigh.textContent = `Average high temperature (celsius) is ${cityData[2].data[5].string_value}`
+  cityDiv.append(cityHigh)
+
+  const cityLow = document.createElement("p")
+  cityLow.textContent = `Average low temperature (celsius) is ${cityData[2].data[6].string_value}`
+  cityDiv.append(cityLow)
+
+  const cityApt = document.createElement("p")
+  cityApt.textContent = `Average large apartment rent in USD is ${cityData[8].data[0].currency_dollar_value}`
+  cityDiv.append(cityApt)
+
+  const cityStartupJobs = document.createElement("p")
+  cityStartupJobs.textContent = `Startup jobs available is ${cityData[10].data[3].int_value}`
+  cityDiv.append(cityStartupJobs)
+
+  const cityStartupSal = document.createElement("p")
+  cityStartupSal.textContent = `Average startup salary in USD is ${cityData[10].data[6].currency_dollar_value}`
+  cityDiv.append(cityStartupSal)
+
+
+  // let cityElements = `
+  // <p>${data}(categories[1].data[0].float_value.Population)population</p>
+  // `
+  // document.querySelector('#city-data').insertAdjacentHTML('beforeend', cityElements)
 
   // data.forEach((city) => {
-  // console.log("Build Elements", data)
-  // const cityDiv = document.createElement("div")
-  //   cityList.append(cityDiv)
     
   // const cityPop = document.createElement("p")
   //   cityPop.textContent = categories[1].data[0].float_value.Population
   //   cityList.append(cityPop)
   // })
-  // return data
+  return cityData
+}
+
+function removeElement(element) {
+  while (element.lastChild) {
+    element.removeChild(element.lastChild)
+  }
 }
